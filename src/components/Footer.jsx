131c09cd89d580e52169378934692c293308e046
Cristhian <cristhian.soto.j@gmail.com>
2022-10-19 16:13:04 -0300

    links v1.2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,27 @@ import MailIcon from "@mui/icons-material/Mail";
 import "./Footer.css";
 
 const Footer = () => {
+      // https://i.ibb.co/Tvv5dJW/Button.png
+// https://i.ibb.co/QMG1RWX/face-logo.png
+// https://i.ibb.co/wLcgYG9/in-logo.png
+// https://i.ibb.co/mBGRVHr/insta-logo.png
+// https://i.ibb.co/1Qsx8qy/iphone-mockup.png
+// https://i.ibb.co/7vg93Lx/Man1.png
+// https://i.ibb.co/WypfnRp/Man2.png
+// https://i.ibb.co/CzH7DhF/message.png
+// https://i.ibb.co/BzStnmP/paso1.png
+// https://i.ibb.co/NY0qfHJ/paso2.png
+// https://i.ibb.co/Z6jXyr8/paso3.png
+// https://i.ibb.co/Z2FjPvs/paso4.png
+// https://i.ibb.co/dtr57Cy/paso5.png
+// https://i.ibb.co/M9Z2Y85/paso6.png
+// https://i.ibb.co/dLDYF11/rocket.png
+// https://i.ibb.co/f91y6K5/SOCIAL-LINKS.png
+// https://i.ibb.co/z8r4MfX/tele-logo.png
+// https://i.ibb.co/4mCqSHC/twitter-logo.png
+// https://i.ibb.co/drg5Jnk/wa-logo.png
+// https://i.ibb.co/FzdZzxV/Woman1.png
+// https://i.ibb.co/m8bNC0H/Woman2.png
   return (
     <Container
       sx={{
@@ -78,7 +99,7 @@ const Footer = () => {
 
         <img
           className="logo"
-          src={"src/assets/insta-logo.png"}
+          src={"https://i.ibb.co/mBGRVHr/insta-logo.png"}
           width={70}
         ></img>
 
@@ -90,7 +111,7 @@ const Footer = () => {
 
         <img
           className="logo"
-          src={"src/assets/twitter-logo.png"}
+          src={"https://i.ibb.co/4mCqSHC/twitter-logo.png"}
           width={70}
         ></img>
 
